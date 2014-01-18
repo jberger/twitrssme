b5e7effb2e8f406a03e357fb700877dcf3eca265
Joel Berger <joel.a.berger@gmail.com>
2014-01-18 17:11:17 -0600

    completed rss transformation

--- twitter_user_to_rss.pl
+++ twitter_user_to_rss.pl
@@ -79,68 +79,17 @@ get '/twitter_user_to_rss' => sub {
   }
 
   my $tweets = $tx->res->dom('li.js-stream-item > div');
-  my $data   = $tweets->map(sub{$self->parse_tweet($_)});  
+  my $items  = $tweets->map(sub{$self->parse_tweet($_)});  
 
-  $self->render( text => $self->dumper($data) );
+  # Cache-control: max-age=$max_age
+  $self->render( 'atom', format => 'rss', user => $user, items => $items );
 };
 
 app->start;
 
-__END__
+__DATA__
 
-while (my $q = CGI::Fast->new) {
-
-	my @items;
-
-	my $tree= HTML::TreeBuilder::XPath->new;
-	$tree->parse($content);
-	my $tweets = $tree->findnodes( '//li' . class_contains('js-stream-item') );  
-
-	for my $li (@$tweets) {    
-		my $tweet = $li->findnodes('./div' 
-																. class_contains("tweet") 
-																. '/div' 
-																. class_contains("content") )->[0]
-		;
-		my $header = $tweet->findnodes('./div' . class_contains("stream-item-header"))->[0];
-		my $body   = $tweet->findvalue('./p' . class_contains("tweet-text"));
-		$body = "<![CDATA[$body]]>";
-		my $avatar = $header->findvalue('./a/img' . class_contains("avatar") . "/\@src"); 
-		my $fullname = $header->findvalue('./a/strong' . class_contains("fullname"));
-		my $username = '@' . $header->findvalue('./a/span' . class_contains("username") . '/b');
-		my $uri = $BASEURL . $header->findvalue('./small' 
-																. class_contains("time") 
-																. '/a'
-																. class_contains("tweet-timestamp") 
-																. '/@href'
-		);  
-		my $timestamp = $header->findvalue('./small' 
-										. class_contains("time") 
-										. '/a'
-										. class_contains("tweet-timestamp") 
-										. '/span/@data-time'
-		);  
-
-		my $pub_date = strftime("%a, %d %b %Y %H:%M:%S %z", localtime($timestamp));
-
-		push @items, {
-			username => $username,
-			fullname => $fullname,
-			link => $uri,
-			guid => $uri,
-			title => $body,
-			description => $body,
-			timestamp => $timestamp,
-			pubDate => $pub_date
-		}
-	}
-	$tree->delete; 
-
-
-	# now print as an rss feed, with header
-print<<ENDHEAD
-Content-type: application/rss+xml
-Cache-control: max-age=$max_age
+@@ atom.rss.ep
 
 <?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:georss="http://www.georss.org/georss" xmlns:twitter="http://api.twitter.com" version="2.0">
@@ -150,44 +99,17 @@ Cache-control: max-age=$max_age
     <description>Twitter feed for: $user.</description>
     <language>en-us</language>
     <ttl>40</ttl>
-ENDHEAD
-;
-
-for (@items) {
-  print<<ENDITEM
-    <item>
-      <title>$_->{username}: $_->{title}</title>
-      <description>$_->{description}</description>
-      <pubDate>$_->{pubDate}</pubDate>
-      <guid>$_->{guid}</guid>
-      <link>$_->{link}</link>
-      <twitter:source/>
-      <twitter:place/>
-    </item>
-ENDITEM
-;
-}
-
-print<<ENDRSS
+    % for (@$items) {
+      <item>
+        <title><%== $_->{title} %></title>
+        <description><%== $_->{description}%></description>
+        <pubDate><%== $_->{pubDate}%></pubDate>
+        <guid><%== $_->{guid}%></guid>
+        <link><%== $_->{link}%></link>
+        <twitter:source/>
+        <twitter:place/>
+      </item>
+    % }
   </channel>
 </rss>      
-ENDRSS
-;
-}
 
-sub class_contains {
-  my $classname = shift;
-  "[contains(concat(' ',normalize-space(\@class),' '),' $classname ')]";
-}
-
-sub err {
-	my $msg = shift;
-	print<<ENDHEAD
-Content-type: text/html
-Cache-control: max-age=86400
-Refresh: 5; url=http://twitrss.me
-
-<html><head></head><body><h2>ERR: $msg</h2><p>Redirecting you back to <a href="http://twitrss.me">TwitRSS.me</a> in a few seconds. You might have spelled the username wrong or something</p></body></html>
-ENDHEAD
-;
-}
